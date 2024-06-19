75593d99361217e30c41b89eb59ff3c52ef64b21
Irina Ichim <onlyirina7@gmail.com>
2024-06-19 02:50:19 +0200

    Fix: Resolved dependency injection issue with EventRepositor in Railway deployment

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,17 +1,22 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventbriteService } from './events.service';
 import { EventsController } from './events.controller';
 import { HttpModule } from '@nestjs/axios';
+import { Event } from './entities/event.entity';
 
 @Module({
   imports: [
     HttpModule.register({
       timeout: 5000,
       maxRedirects: 5,
-    })
+    }),
+    TypeOrmModule.forFeature([Event]),  
   ],
   controllers: [EventsController],
   providers: [EventbriteService],
+  exports: [EventbriteService],  
 })
 export class EventsModule {}
+
