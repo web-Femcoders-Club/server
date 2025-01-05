bdfe45ed3de77e34d0d60d00410e329bf09f4480
Irina Ichim <onlyirina7@gmail.com>
2025-01-05 02:26:13 +0100

    update email-formulario.dto

--- src/email-formulario/dto/email-formulario.dto.ts
+++ src/email-formulario/dto/email-formulario.dto.ts
@@ -1,11 +1,26 @@
-import { IsNotEmpty, IsEmail } from 'class-validator';
+// import { IsNotEmpty, IsEmail } from 'class-validator';
+
+// export class ContactFormDto {
+//   @IsNotEmpty()
+//   name: string;
+
+//   @IsNotEmpty()
+//   lastName: string;
+
+//   @IsEmail()
+//   email: string;
+
+//   @IsNotEmpty()
+//   message: string;
+// }
+import { IsNotEmpty, IsEmail, IsOptional } from 'class-validator';
 
 export class ContactFormDto {
   @IsNotEmpty()
   name: string;
 
-  @IsNotEmpty()
-  lastName: string;
+  @IsOptional()
+  lastName?: string;
 
   @IsEmail()
   email: string;
